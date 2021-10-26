99580a86c118c6e47e88251b5961ca8ddbf17b4a
Josh <jgordon1401@gmail.com>
2021-10-26 12:31:06 -0400

    Added a input file python script; compatmentalized everything into seperate folders; updated makefile to work with changes

--- Makefile
+++ Makefile
@@ -1,25 +1,27 @@
-CFLAGS = g++ -g -c
+CFLAGS = g++ -g -I $(HEADERSPATH) -c $(SRCPATH)
+SRCPATH = src/
+HEADERSPATH = headers/
 #Makefile for CP3
 
 all:
 
-Activity.o: Activity.cpp
-	$(CFLAGS) Activity.cpp -o Activity.o 
+Activity.o: $(SRCPATH)Activity.cpp
+	$(CFLAGS)Activity.cpp -o Activity.o 
 
-LinkedList.o: LinkedList.cpp
-	$(CFLAGS) LinkedList.cpp -o LinkedList.o
+LinkedList.o: $(SRCPATH)LinkedList.cpp
+	$(CFLAGS)LinkedList.cpp -o LinkedList.o
 
-User.o: User.cpp
-	$(CFLAGS) User.cpp -o User.o
+User.o: $(SRCPATH)User.cpp
+	$(CFLAGS)User.cpp -o User.o
 
 #Test target
 
 test: Activity.o LinkedList.o User.o Test.o
 	g++ Activity.o LinkedList.o Test.o User.o -o test.exe
 
-Test.o: Test.cpp
-	$(CFLAGS) Test.cpp -o Test.o 
-	
+Test.o: $(SRCPATH)Test.cpp
+	$(CFLAGS)Test.cpp -o Test.o 
+
 # Specify the object files and executables that are generated
 # and need to be removed to re-compile the whole thing
 clean:
---
+++ StringCombinerScript/VenueCombiner.py
@@ -0,0 +1,28 @@
+from os import stat
+import random
+
+venueList = open("/home/josh/Documents/CS240/CP3/StringCombinerScript/ListOfVenues.txt", 'r')
+statesList = open("/home/josh/Documents/CS240/CP3/StringCombinerScript/ListOfStates.txt", 'r')
+
+listOfVenues = venueList.readlines()
+for i in listOfVenues:
+    currVenue = i
+    currVenue = currVenue.strip()
+    currVenue = currVenue.replace("\n", "")
+    currVenue = currVenue.replace(" ", "_")
+    listOfVenues[listOfVenues.index(i)] = currVenue
+
+listOfStates = statesList.readlines()
+for i in listOfStates:
+    currState = i.strip()
+    listOfStates[listOfStates.index(i)] = currState.replace("\n", "")
+venueFileFinal = open("/home/josh/Documents/CS240/CP3/storageFiles/VenueFile.txt", 'w')
+
+for i in range(len(listOfVenues)):
+    numberOfPeople =  str(random.randint(300, 10000))
+    #print(listOfVenues[i] + numberOfPeople + " " + listOfStates[i])
+    venueFileFinal.write(listOfVenues[i] + " " + numberOfPeople + " " + listOfStates[i] + "\n")
+
+venueList.close()
+statesList.close()
+venueFileFinal.close()
